da01c6fb4f2bd17999a155a6c88fd97b14564b89
Ｇ＆Ｔ <garrashtornado15190107@gmail.com>
2016-10-03 14:16:14 +0900

    C#学習開始
    ・C#フォルダ作成
    ・C#README作成
    ・.gitignore更新
    ・p004まで追加

---
+++ C#/p001/p001/Program.cs
@@ -0,0 +1,8 @@
+﻿class test
+{
+    static void Main()
+    {
+        System.Console.WriteLine("ほあようごあいまーしゅ！");
+        for (; ; ) ;                                            //オートフォーマットされてしまうらしい。そういう理になっている。
+    }
+}
---
+++ C#/p003/p003/Program.cs
@@ -0,0 +1,18 @@
+﻿class test
+{
+    static void Main()
+    {
+        System.Console.WriteLine(
+@"（・∀・）
+ここから
+はじまり
+そして
+ここで
+おわる
+（・∀・）
+"
+        );
+        System.Console.WriteLine("こんなことするとソースコード無茶苦茶見にくくならないですか？？？？");
+        for (; ; ) ;
+    }
+}
