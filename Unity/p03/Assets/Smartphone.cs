c90b0739fa73ac5bfbb6075214c289ee6d1ac7e7
Ｇ＆Ｔ <garrashtornado15190107@gmail.com>
2017-05-10 11:57:05 +0900

    Unity追加
    - 玉落とし（完成）
        * 各種オブジェクトの作成
        * C#スクリプトの作成

---
+++ Unity/p03/Assets/Smartphone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smartphone : MonoBehaviour {
+
+	// 毎フレーム処理
+	void Update () {
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            // スマートフォンの重力を取得（varで型指定不要）
+            var direction = Input.acceleration;
+
+            // z方向加速度を無視
+            direction.z = 0;
+
+            // 重力を正規化した重力方向に変換
+            Vector3 v = direction.normalized;
+            Physics.gravity = v;
+
+            // 変換後の重力に従った計算は他のどこかでやってくれる模様
+        }
+	}
+}
