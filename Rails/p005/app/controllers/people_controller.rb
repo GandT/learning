293d21af5f9df31fd4b65d6f22507245cc86eb90
Ｇ＆Ｔ <garrashtornado15190107@gmail.com>
2017-06-01 12:18:02 +0900

    Rails学習
    - データベースの利用
        * 疑似名前データベース
        * Create/Read/Update/Deleteの一通りの演習

--- Rails/p005/app/controllers/people_controller.rb
+++ Rails/p005/app/controllers/people_controller.rb
@@ -2,6 +2,56 @@ class PeopleController < ApplicationController
   # 表示データのセット
   def index
     @msg = "個人情報"
+    # 全データ取得
     @data = Person.all
+  end
+
+  # 表示
+  def show
+    @msg = "詳細情報"
+    # 対応id番号の個人を取得
+    @data = Person.find(params[:id])
+  end
+
+  # 追加メッセージ
+  def add
+    @msg = "新しいデータを追加しまつ。"
+    @person = Person.new
+  end
+
+  # データ生成
+  def create
+    if request.post?
+      obj = Person.create(person_params)
+    end
+    redirect_to '/people'
+  end
+
+  # 編集
+  def edit
+    @msg = "データを編集しまつ【id = #{params[:id]}】"
+    @person = Person.find(params[:id])
+  end
+
+  # 編集情報を反映
+  def update
+    obj = Person.find(params[:id])
+    obj.update(person_params)
+    redirect_to '/people'
+  end
+
+  # 削除
+  def delete
+    obj = Person.find(params[:id])
+    obj.destroy
+    redirect_to '/people'
+  end
+
+
+  # 以下プライベート
+  private
+  # パラメータ情報が完備であることを保証
+  def person_params
+    params.require(:person).permit(:name, :age, :mail)
   end
 end
--- Rails/p005/config/routes.rb
+++ Rails/p005/config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  get 'people/index'
+  get   'people/index'
+  get   'people', to: "people#index"
+
+  get   'people/add'
+  post  'people/add', to: 'people#create'
+
+  get   'people/edit/:id', to: 'people#edit'
+  post  'people/edit/:id', to: 'people#update'
+  patch 'people/edit/:id', to: 'people#update'
+
+  get   'people/delete/:id', to: 'people#delete'
+
+  get   'people/id'
+  get   'people/:id', to: 'people#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
