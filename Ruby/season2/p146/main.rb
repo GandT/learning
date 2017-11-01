160c3427156f626c1f897ba773af189187c8d68f
GandT <garrashtornado15190107@gmail.com>
2017-11-01 17:50:46 +0900

    RubyにおけるCSVの練習
    - CSVファイルを開く
        * foreachのみ練習

---
+++ Ruby/season2/p146/main.rb
@@ -0,0 +1,15 @@
+#coding: UTF-8
+=begin
+　2017.11.1
+　Ruby CSV
+
+　【参考文献】
+　shizuma（2016）「Ruby CSVを扱うためのメモ」 <https://qiita.com/shizuma/items/7719172eb5e8c29a7d6e> 2017年11月1日アクセス.
+=end
+
+require "csv"
+
+data = CSV.foreach('data.csv', headers: true){ |d|
+    puts "#{d["age"]}年間#{d["station"]}駅にいる#{d["name"]}という人物を探して欲しい。"
+}
+
