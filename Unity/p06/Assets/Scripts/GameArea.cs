9cfc2238becfac375bbc52a14129f16c98f3651f
GandT <GandT.tornado@gmail.com>
2018-03-21 00:05:30 +0900

    Unity: チュートリアル「Space Shooter」
    2-1. Boundary

---
+++ Unity/p06/Assets/Scripts/GameArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameArea : MonoBehaviour
+{
+	// 領域からオブジェクトが離れた際に呼び出される
+	void OnTriggerExit(Collider other)
+	{
+        Destroy(other.gameObject);
+    }
+}
