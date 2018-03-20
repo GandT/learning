94e909f414684bed8192b5bb4e28d07707a84753
GandT <GandT.tornado@gmail.com>
2018-03-20 18:54:31 +0900

    Unity: チュートリアルのディレクトリ構造変更
    - Scriptフォルダの位置を修正

---
+++ Unity/p06/Assets/Scripts/ShotController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+	Rigidbody _rigidbody;
+	Transform _transform;
+
+	float speed = 20;
+
+	void Start () {
+		// アクセスするオブジェクトの取得
+		_rigidbody = GetComponent<Rigidbody>();
+		_transform = GetComponent<Transform>();
+
+		// パラメータ設定
+		_rigidbody.velocity = _transform.forward * speed;
+	}
+
+
+	void Update () {
+		
+	}
+}
