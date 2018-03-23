e9c4206965832ded1913e4df65d1ca6ecc3f3877
GandT <GandT.tornado@gmail.com>
2018-03-23 20:23:35 +0900

    Unity: チュートリアル「Space Shooter」
    2-4. Game Controller

---
+++ Unity/p06/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	[SerializeField] GameObject hazard = null;
+	[SerializeField] Vector3 spawnValues = new Vector3(6, 0, 16);
+
+	void Start()
+	{
+		// 初期化
+
+		// 敵を作成
+		SpawnWaves();
+	}
+	
+
+	void Update()
+	{
+		
+	}
+
+
+	void SpawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity; // 無回転
+
+		Instantiate(hazard, spawnPosition, spawnRotation);
+	}
+}
