c48ad075d511256979d1a38a9ade178781d344ce
GandT <GandT.tornado@gmail.com>
2018-03-25 02:46:25 +0900

    Unity: チュートリアル「Space Shooter」
    2-5. Spawning waves

---
+++ Unity/p06/Assets/Scripts/DestroyController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+	[SerializeField] float lifetime = 0;
+
+	void Start () {
+		Destroy(gameObject, lifetime);
+	}
+}
--- Unity/p06/Assets/Scripts/GameController.cs
+++ Unity/p06/Assets/Scripts/GameController.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	[SerializeField] GameObject hazard = null;
-	[SerializeField] Vector3 spawnValues = new Vector3(6, 0, 16);
+
+	Vector3 spawnValues = new Vector3(6, 0, 16);
+	int hazardCount = 10;
+	float spawnWait = 0.5f, startWait = 1, waveWait = 4;
 
 	void Start()
 	{
 		// 初期化
 
 		// 敵を作成
-		SpawnWaves();
+		StartCoroutine(SpawnWaves());
 	}
 	
 
@@ -22,11 +25,21 @@ public class GameController : MonoBehaviour {
 	}
 
 
-	void SpawnWaves()
+	IEnumerator SpawnWaves()
 	{
-		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity; // 無回転
+		yield return new WaitForSeconds(startWait);
 
-		Instantiate(hazard, spawnPosition, spawnRotation);
+		while(true){ 
+			for(int i = 0; i < hazardCount; ++i){
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity; // 無回転
+
+				Instantiate(hazard, spawnPosition, spawnRotation);
+
+				yield return new WaitForSeconds(spawnWait);
+			}
+
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }
