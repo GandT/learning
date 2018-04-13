89ed612e1a23f28fdc2246efd1b14fb7e8bd67d2
GandT <GandT.tornado@gmail.com>
2018-04-13 17:41:07 +0900

    Python: 入門課題
    - 遺伝子配列のエンコード

---
+++ Python/season2/p014/p014.py
@@ -0,0 +1,25 @@
+# coding: UTF-8
+"""
+　2018.4.12
+　遺伝子配列のエンコード
+"""
+
+def atgc_encode(s):
+    # 個数を数えてハッシュ化
+    a, t, g, c = s.count("A"), s.count("T"), s.count("G"), s.count("C")
+    atgc = {a: "A", t: "T", g: "G", c:"C"}
+
+    # ソート
+    atgc = sorted(atgc.items())
+    
+    # 変換テーブルの作成
+    encoder = {atgc[0][1]: "4", atgc[1][1]: "3", atgc[2][1]: "2", atgc[3][1]: "1"}
+
+    result = ""
+    for c in s:
+        result += encoder[c]
+
+    return result
+
+print(atgc_encode("AAGCCCCATGGTAA"))
+print(atgc_encode("AAAATTTTGGCCCCCAAAAA"))
