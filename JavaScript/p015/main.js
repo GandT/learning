8b17ec0ded54757379e54466ce98483052811280
GandT <GandT.tornado@gmail.com>
2019-03-12 23:05:08 +0900

    JavaScript: dotinstall授業分追加
    - ポップアップウィンドウ表示系関数
    - 関数の各種作成方法

---
+++ JavaScript/p015/main.js
@@ -0,0 +1,15 @@
+﻿/*
+    2019.3.12
+    ポップアップウィンドウ表示系関数
+*/
+
+
+let result;
+
+alert("alert");
+
+result = confirm("confirm");
+alert(result + "でした。");
+
+result = prompt("prompt", "default");
+alert(result + "でした。");
